365a75dd84ea016850982a9079cf3a044c6db668
Erriez <Erriez@users.noreply.github.com>
2023-01-14 12:37:37 +0100

    Add comments to qtimer_slingshot example

--- 00_qt_core/01_qtimer_singleshot.py
+++ 00_qt_core/01_qtimer_singleshot.py
@@ -23,19 +23,37 @@
 #
 # Source: https://github.com/Erriez/pyside6-getting-started
 #
+# This example creates a Qt core application without a GUI and quits after 3
+# seconds.
+#
 
+# Import PySide6 modules
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QTimer
+
+# Import Python system module
 import sys
 
 
+# Main function
 def main():
+    # Print message on a terminal
     print('Waiting to quit application...')
 
+    # Create QApplication object
+    # https://doc.qt.io/qtforpython/PySide6/QtWidgets/QApplication.html
     app = QApplication([])
+
+    # Close application after 3 seconds
+    # https://doc.qt.io/qtforpython-6/PySide6/QtCore/QTimer.html
     QTimer.singleShot(3000, app.quit)
+
+    # Qt application loop runs until application is quit.
+    # Exit code 0 (success) is returned to the console on application quit.
+    # Exit codes other than 0 are returned when an error occurred.
     sys.exit(app.exec())
 
 
+# Python code starts here
 if __name__ == "__main__":
     main()
